a1ae309b74419e74396f3cd7b175776b119fbd55
Gordon Nongkynrih <gordon@dolphinze.com>
2026-03-12 10:22:03 +0530

    12-03-2026

--- resources/views/components/admin/⚡table-management.blade.php
+++ resources/views/components/admin/⚡table-management.blade.php
@@ -10,27 +10,48 @@ new class extends Component
     public $selectedId = null;
     public $name = '';
     public $status = 'available';
+    public $search='';
     public $tableStatus = true;
     public $tables = [];
     public $headers = [
         ['key' => 'name', 'label' => 'Table Name'],
         ['key' => 'status', 'label' => 'Status'],
     ];
-
+    public $isEditing;
     public $showInputForm;
     public $showConfirmDeleteModal;
     //mount gets executed when the component is loaded
     public function mount(){
         $this->showInputForm = false;
         $this->showConfirmDeleteModal = false;
+        $this->isEditing = false;
+        $this->search = '';
+        $this->getTables();
+    }
+   
+    //this function is called when the search input is updated
+    public function updatedSearch()
+    {
         $this->getTables();
     }
     public function getTables()
     {
         //select * from restaurant_tables
-        $this->tables = RestaurantTable::all();
+        // $this->tables = RestaurantTable::all();
         //select * from restaurant_tables limit 3
         // $this->tables = RestaurantTable::paginate(3);
+        
+        if(!empty($this->search)) {
+            //select * from restaurant_tables where name like '%search%'
+            $this->tables = RestaurantTable::where('name', 'like', '%'.$this->search.'%')->get();
+        } else {
+            $this->tables = RestaurantTable::all();
+        }
+    }
+    public function create()
+    {
+        $this->showInputForm = true;
+        $this->isEditing = false;
     }
     public function store()
     {
@@ -66,18 +87,73 @@ new class extends Component
     public function confirmDelete($id)
     {
         $this->showConfirmDeleteModal = true;
+        $this->selectedId = $id; //store the id of the record to be deleted
     }
 
     public function edit($id)
     {
         $this->showInputForm = true;
         $this->selectedId = $id;
-
+        $this->isEditing = true;
         //select * from restaurant_tables where id = $id
         $table = RestaurantTable::find($id); //use find if searching by primary key
+        if(!$table) { //if the table does not exist show error
+            $this->toast(
+                type: 'error',
+                css: 'bg-red-200 text-gray-700',
+                title: 'Error updating record!',
+                description: 'Table not found!',
+            );
+            $this->isEditing = false;
+            $this->showInputForm = false;
+            return;
+        }
         $this->name = $table->name;
         $this->status = $table->status;
         $this->tableStatus = $table->status == 'available' ? true : false;
+        $this->isEditing = true;
+    }
+
+    public function update()
+    {
+        // RestaurantTable::where('id', $this->selectedId)->update([
+        //     'name' => $this->name,
+        //     'status' => $this->status,
+        // ]);
+        //select * from restaurant_tables where id = $this->selectedId limit 1
+        $table = RestaurantTable::where('id', $this->selectedId)->first();
+        if(!$table) { //if the table does not exist show error
+            $this->toast(
+                type: 'error',
+                css: 'bg-red-200 text-gray-700',
+                title: 'Error updating record!',
+                description: 'Table not found!',
+            );
+            return;
+        }
+        $table->name = $this->name;
+        $table->status = $this->status;
+        $table->save();
+
+        $this->getTables();
+        $this->showInputForm = false;
+        $this->isEditing = false;
+        $this->toast(
+            type: 'success',
+            css: 'bg-white text-gray-700',
+            title: 'Record updated successfully!',
+        );
+    }
+    public function delete()
+    {
+        RestaurantTable::where('id', $this->selectedId)->delete();
+        $this->getTables();
+        $this->showConfirmDeleteModal = false;
+        $this->toast(
+            type: 'success',
+            css: 'bg-white text-gray-700',
+            title: 'Record deleted successfully!',
+        );
     }
 };
 ?>
@@ -87,19 +163,11 @@ new class extends Component
         
     </div>
 
-    <x-modal wire:model="showInputForm" title="Table">
-        <x-form wire:submit="store" 
-            class=" bg-white p-4 rounded-2xl shadow-2xl">
-            <x-input wire:model="name" label="Name" />
-            <x-toggle label="Available" wire:model="tableStatus" right />
-            <div class="flex justify-between">
-                <x-button label="Close" @click="$wire.showInputForm = false" />
-                <x-button label="Save" class="btn-primary" type="submit" spinner="save" />
-            </div>
-        </x-form>
-    </x-modal>
-
-    <x-button icon="o-plus" label="Add New Table" @click="$wire.showInputForm = true" />
+    <div class="flex gap-4">
+        <x-button icon="o-plus" label="Add New Table" wire:click="create" />
+        <x-input wire:model.live.debounce.300ms="search" placeholder="Search tables..." icon="o-magnifying-glass" />
+        {{-- <x-button icon="o-arrow-path" label="Search" wire:click="getTables" /> --}}
+    </div>
 
     <x-table :headers="$headers"  :rows="$tables">
         @scope('cell_status', $table)
@@ -109,23 +177,31 @@ new class extends Component
                 <x-badge value="Occupied" class="badge-error" />
             @endif
         @endscope
-
-       
         @scope('actions', $table)
             <div class="flex gap-4">
-                <x-button icon="o-pencil" class="text-blue-500" wire:click="edit({{ $table->id }})" />
-                <x-button icon="o-trash" class="text-red-500" wire:click="confirmDelete({{ $table->id }})" />
+                <x-button icon="o-pencil" class="bg-indigo-400 text-white btn-circle" wire:click="edit({{ $table->id }})" />
+                <x-button icon="o-trash" class="text-white bg-red-400 btn-circle" wire:click="confirmDelete({{ $table->id }})" />
             </div>
         @endscope
     </x-table>
     
-
+     <x-modal wire:model="showInputForm" title="Table">
+        <x-form wire:submit="{{ $isEditing == true ? 'update' : 'store' }}" 
+            class=" bg-white p-4 rounded-2xl shadow-2xl">
+            <x-input wire:model="name" label="Name" />
+            <x-toggle label="Available" wire:model="tableStatus" right />
+            <div class="flex justify-between">
+                <x-button label="Close" @click="$wire.showInputForm = false" />
+                <x-button label="{{ $isEditing == true ? 'Update' : 'Save' }}" class="btn-primary" type="submit" spinner="save" />
+            </div>
+        </x-form>
+    </x-modal>
     <x-modal wire:model="showConfirmDeleteModal" title="Delete" class="backdrop-blur">
     Are you sure you want to delete this table?
 
     <x-slot:actions>
         <x-button label="No" @click="$wire.showConfirmDeleteModal = false" />
-        <x-button label="Yes" class="btn-error" @click="$wire.showConfirmDeleteModal = false" />
+        <x-button label="Yes" class="btn-error" wire:click="delete" />
     </x-slot:actions>
 </x-modal>
 </div>
